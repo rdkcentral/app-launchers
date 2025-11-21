4c231593f1a34cce893f5bda6b0e7f71af11a1d7
Lukasz Iwan <lukasz_iwan@comcast.com>
2025-11-21 14:36:47 +0100

    feat: reading and passing most of cobalt config

--- src/CobaltLauncher/CobaltConfig.h
+++ src/CobaltLauncher/CobaltConfig.h
@@ -28,18 +28,131 @@ public:
         return { false, "" };
     }
     // TODO: implement, reducing boilerplate
-    std::pair<bool, std::string> GetClientIdentifier();
-    std::pair<bool, std::string> GetLanguage();
-    std::pair<bool, std::string> GetContentDir();
-    std::pair<bool, std::string> GetGstDebug();
-    std::pair<bool, std::string> GetEssosContextDestroy();
-    std::pair<bool, bool> GetPreloadEnabled();
-    std::pair<bool, uint16_t> GetAutoSuspendDelay();
-    std::pair<bool, std::string> GetClosurePolicy();
-    std::pair<bool, std::string> GetFireboltEndpoint();
+    std::pair<bool, std::string> GetClientIdentifier()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.clientidentifier")) {
+                return { true, m_config->GetString("configuration.clientidentifier") };
+            }
+            return { false, "" };
+        }
 
-    std::pair<bool, std::string> GetSystemProperties();
-    std::pair<bool, std::string> GetAdvertisingId();
+        return { false, "" };
+    }
+    std::pair<bool, std::string> GetLanguage()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.language")) {
+                return { true, m_config->GetString("configuration.language") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+    std::pair<bool, std::string> GetContentDir()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.contentdir")) {
+                return { true, m_config->GetString("configuration.contentdir") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+    std::pair<bool, std::string> GetGstDebug()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.gstdebug")) {
+                return { true, m_config->GetString("configuration.gstdebug") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+    std::pair<bool, std::string> GetEssosContextDestroy()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.essoscontextdestroy")) {
+                return { true, m_config->GetString("configuration.essoscontextdestroy") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+    std::pair<bool, bool> GetPreloadEnabled()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.preload")) {
+                return { true, m_config->GetBoolean("configuration.preload") };
+            }
+            return { false, false };
+        }
+
+        return { false, false };
+    }
+    std::pair<bool, uint16_t> GetAutoSuspendDelay()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.autosuspenddelay")) {
+                return { true, m_config->GetNumber("configuration.autosuspenddelay") };
+            }
+            return { false, 0 };
+        }
+
+        return { false, 0 };
+    }
+    std::pair<bool, std::string> GetClosurePolicy()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.closurepolicy")) {
+                return { true, m_config->GetString("configuration.closurepolicy") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+    std::pair<bool, std::string> GetFireboltEndpoint()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.fireboltendpoint")) {
+                return { true, m_config->GetString("configuration.fireboltendpoint") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+
+    // FIXME: Get all the fields as JSON string
+    std::pair<bool, std::string> GetSystemProperties()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.systemproperties")) {
+                return { true, m_config->GetString("configuration.systemproperties") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
+
+    // FIXME: Get all the fields as JSON string
+    std::pair<bool, std::string> GetAdvertisingId()
+    {
+        if (m_config) {
+            if (m_config->Contains("configuration.advertisingid")) {
+                return { true, m_config->GetString("configuration.advertisingid") };
+            }
+            return { false, "" };
+        }
+
+        return { false, "" };
+    }
     // TODO: SbMainArgs
 private:
     IConfig* m_config;
--- src/CobaltLauncher/CobaltLauncher.cpp
+++ src/CobaltLauncher/CobaltLauncher.cpp
@@ -1,5 +1,76 @@
 #include "CobaltLauncher.h"
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#ifdef MOCK_SB
+extern "C" {
+
+int StarboardMain(int argc, char** argv)
+{
+    return 0;
+}
+void SbRdkHandleDeepLink(const char* link)
+{
+}
+void SbRdkSuspend()
+{
+}
+void SbRdkResume()
+{
+}
+void SbRdkPause()
+{
+}
+void SbRdkUnpause()
+{
+}
+void SbRdkQuit()
+{
+}
+void SbRdkSetSetting(const char* key, const char* json)
+{
+}
+int SbRdkGetSetting(const char* key, char** out_json)
+{
+    return 0;
+}
+
+typedef int (*SbRdkCallbackFunc)(void* user_data);
+void SbRdkSetConcealRequestHandler(SbRdkCallbackFunc cb, void* user_data)
+{
+}
+void SbRdkSetCobaltExitStrategy(const char* strategy)
+{
+}
+
+} // extern "C"
+#else
+extern "C" {
+
+int StarboardMain(int argc, char** argv);
+void SbRdkHandleDeepLink(const char* link);
+void SbRdkSuspend();
+void SbRdkResume();
+void SbRdkPause();
+void SbRdkUnpause();
+void SbRdkQuit();
+void SbRdkSetSetting(const char* key, const char* json);
+int SbRdkGetSetting(const char* key, char** out_json);
+
+typedef int (*SbRdkCallbackFunc)(void* user_data);
+void SbRdkSetConcealRequestHandler(SbRdkCallbackFunc cb, void* user_data);
+void SbRdkSetCobaltExitStrategy(const char* strategy);
+
+} // extern "C"
+#endif
+
+namespace {
+void SetEnvironment(const std::string& key, const std::string& value, bool forced = false)
+{
+    ::setenv(key.c_str(), value.c_str(), forced);
+}
+}
 
 CobaltLauncher::CobaltLauncher(ILifeCycle* lifecycle)
     : m_lifecycle(lifecycle)
@@ -12,28 +83,71 @@ CobaltLauncher::CobaltLauncher(ILifeCycle* lifecycle)
 int CobaltLauncher::Run()
 {
     std::cout << "Cobalt starting...\n";
+    std::vector<const char*> argv;
+    argv.push_back("Cobalt");
 
-    auto result = m_config.GetUrl();
-    if (result.first) {
-        std::cerr << "Launchung with url: " << result.second << "\n";
-    } else {
-        std::cerr << "Launchung without url \n";
+    auto url = m_config.GetUrl();
+    if (url.first) {
+        std::cout << "Launchung with url: " << url.second << "\n";
+        argv.push_back(url.second.c_str());
     }
 
-    // simulate blocking call to SbRdkMain
+    auto preload = m_config.GetPreloadEnabled();
+    if (preload.first && preload.second) {
+        argv.push_back("--preload");
+    }
+
+    int result = -1;
     while (m_isRunning) {
+        result = StarboardMain(argv.size(), const_cast<char**>(argv.data()));
     }
-    return 1;
+    return result;
 }
 
 bool CobaltLauncher::Configure(IConfig* config)
 {
     std::cout << "Cobalt configuring...\n";
-    return m_config.Configure(config);
+
+    if (!m_config.Configure(config)) {
+        return false;
+    }
+
+    auto language = m_config.GetLanguage();
+    if (language.first) {
+        SetEnvironment("LANG", language.second);
+    }
+
+    auto essosContextDestroy = m_config.GetEssosContextDestroy();
+    if (essosContextDestroy.first) {
+        SetEnvironment("COBALT_ESSOS_CONTEXT_DESTROY", essosContextDestroy.second);
+    }
+
+    auto autoSuspendDelay = m_config.GetAutoSuspendDelay();
+    if (autoSuspendDelay.first) {
+        // TODO: Schedule auto suspend;
+    }
+
+    auto systemProperties = m_config.GetSystemProperties();
+    if (systemProperties.first) {
+        SbRdkSetSetting("systemproperties", systemProperties.second.c_str());
+    }
+
+    auto advertisingId = m_config.GetAdvertisingId();
+    if (advertisingId.first) {
+        SbRdkSetSetting("advertisingId", advertisingId.second.c_str());
+    }
+
+    auto fireboltEndpoint = m_config.GetFireboltEndpoint();
+    if (fireboltEndpoint.first) {
+        SetEnvironment("FIREBOLT_ENDPOINT", fireboltEndpoint.second);
+    }
+
+    return true;
 }
 
 void CobaltLauncher::Quit()
 {
-    m_isRunning = false;
     std::cout << "QUIT\n";
+    m_isRunning = false;
+    SbRdkQuit();
 }
